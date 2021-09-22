91b5f0fca6d2b9bc7ba7a899cc80dd8f6c6c13a5
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-22 23:17:20 +0200

    add getAllFrenchConfirmedCases to French Controller

--- src/main/java/de/neuefische/covidcases/Controller/CovidCasesController_France.java
+++ src/main/java/de/neuefische/covidcases/Controller/CovidCasesController_France.java
@@ -1,9 +1,28 @@
 package de.neuefische.covidcases.Controller;
 
+import de.neuefische.covidcases.Model.ModelTotalCovidCase;
+import de.neuefische.covidcases.Service.CovidCasesService_France;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("covidCases/France")
 public class CovidCasesController_France {
+
+    private final CovidCasesService_France covidCasesService_france;
+
+    @Autowired
+    public CovidCasesController_France(CovidCasesService_France covidCasesService_france) {
+        this.covidCasesService_france = covidCasesService_france;
+    }
+
+    @GetMapping
+    public List<ModelTotalCovidCase>getAllFrenchConfirmedCases(){
+        return covidCasesService_france.getFrenchConfirmedCases();
+    }
+
 }
