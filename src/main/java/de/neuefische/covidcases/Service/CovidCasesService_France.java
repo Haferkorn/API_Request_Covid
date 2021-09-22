950611bc787fd5748c9c771acf76e83ba245389d
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-09-22 23:08:00 +0200

    add getFrenchCasesPerDay Method

--- src/main/java/de/neuefische/covidcases/Service/CovidCasesService_France.java
+++ src/main/java/de/neuefische/covidcases/Service/CovidCasesService_France.java
@@ -1,9 +1,14 @@
 package de.neuefische.covidcases.Service;
 
+import de.neuefische.covidcases.API.APIModel;
 import de.neuefische.covidcases.API.France.APIService_France;
 import de.neuefische.covidcases.Mapper.CasesPerDayMapper;
 import de.neuefische.covidcases.Mapper.TotalCovidCasesMapper;
+import de.neuefische.covidcases.Model.ModelCasesPerDay;
+import de.neuefische.covidcases.Model.ModelTotalCovidCase;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CovidCasesService_France {
@@ -16,6 +21,12 @@ public class CovidCasesService_France {
         this.casesPerDayMapper = casesPerDayMapper;
         this.apiService_france = apiService_france;
     }
+
+    public List<ModelTotalCovidCase> getFrenchCasesPerDay(){
+        APIModel[] apiModels=apiService_france.getCovidApiCountryPerDay();
+        return totalCovidCasesMapper.mapToConfirmedByDay(apiModels);
+    }
+
 
 
 }
